acf2cae315df3f3b81509a87d18f7948510a6ed3
Denis Rumyantsev <v-denisr@microsoft.com>
2021-08-02 07:21:46 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace checkOS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Operating system is {OSPlatform.Windows}: {RuntimeInformation.IsOSPlatform(OSPlatform.Windows)}");
+            Console.WriteLine($"Operating system is {OSPlatform.Linux}:   {RuntimeInformation.IsOSPlatform(OSPlatform.Linux)}");
+            Console.WriteLine($"Operating system is {OSPlatform.OSX}:     {RuntimeInformation.IsOSPlatform(OSPlatform.OSX)}");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+Steps:
+
+* Install [dotnet 2.1](https://dotnet.microsoft.com/download/dotnet/2.1)
+
+* Run next commands via terminal:
+  * `dotnet new console --force`
+  * `git restore .`
+  * `dotnet run`
